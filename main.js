b324d07e1b93e2c8e6ede808f77f8f07b10aece6
Jason Bach <526JBach@frhsd.com>
2025-11-26 12:38:30 -0500

    Event Handling

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             image: 'assets/images/socks_green.jpg',
-            url: 'google.com',
+            url: 'https://google.com',
             inventory: 100,
             onSale: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color: 'green'},
-                {id: 2235, color: 'blue'},
+                { id: 2234, color: 'green', image: 'assets/images/socks_green.jpg'},
+                { id: 2235, color: 'blue', image: 'assets/images/socks_blue.jpg'},
             ],
-            sizes: ['Small', "Medium", "Large", "Micheal Forgione"]
+            sizes: ['Small', 'Medium', 'Large']
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
+        removeCart() {
+            this.cart -= 1
         }
     }
 })
